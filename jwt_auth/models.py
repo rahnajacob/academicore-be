93b7194a6743319bd09f53b103d897361a607fb3
rjacob <rahnarjacob@gmail.com>
2024-08-25 17:54:10 +0530

    made first migrations

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    teach_fname = models.CharField(max_length=50)
+    teach_lname = models.CharField(max_length=50)
+    teach_dob = models.DateField()
+
+    def __str__(self):
+        return f'{self.teach_fname} {self.teach_lname}'
